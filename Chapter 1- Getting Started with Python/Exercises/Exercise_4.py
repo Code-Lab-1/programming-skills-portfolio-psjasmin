36f8a298985a3afde4f9fc814cf916b76f39aa9a
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-14 12:27:31 +0400

    Create Exercise_4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
@@ -0,0 +1,10 @@
+## Solutions for Chapter 1: Getting Started with Python
+
+# Exercise 4: Strings Concatination
+# Write three strings in different variables and print the output as one string.
+
+name = "Princess"
+favorite_color = "yellow"
+hobby = "reading"
+
+print("\n" + name + " loves the color " + favorite_color + " and enjoys " + hobby + "\n")
