ca873131fbff3d8dae972b3e810ca232a744fd08
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-15 16:02:04 +0400

    Delete Exercise_2.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_2.py
+++
@@ -1,9 +0,0 @@
-## Solutions for Chapter 1: Getting Started with Python
-
-# Exercise 2: Print the Version of Python
-# Write a Python program to get the Python version you are using.
-
-import sys #system-specific function
-print("\nYour current version of Python is: ", sys.version) #prints the version
-print("Information of the version: ", sys.version_info) #prints details of the version
-print()
