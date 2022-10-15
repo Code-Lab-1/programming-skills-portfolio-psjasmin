bb895cc5f9345f0f42dd2eeb63e6e682f07e6372
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-15 15:52:05 +0400

    Create Exercise_3_Stripping Names.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_3_Stripping Names.py
@@ -0,0 +1,17 @@
+## Solutions for Chapter 2: Variables & Comments
+
+# Exercise 3: Stripping Names
+# Tidy up the code to make it easier to understand
+
+name = " \tArcher Hale\n "
+print("\nOriginal:")
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+print("\nUsing rstrip():")
+print(name.rstrip())
+print("\nUsing strip():")
+print(name.strip())
+
+print()
