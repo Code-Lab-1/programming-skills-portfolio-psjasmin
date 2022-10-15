cacbff2de49ce3864718ce3661659bb4d980ff16
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-15 15:51:49 +0400

    Create Exercise_2_Variables.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_2_Variables.py
@@ -0,0 +1,8 @@
+## Solutions for Chapter 2: Variables & Comments
+
+# Exercise 2: Variables
+# Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+famous_person = "\nOscar Wilde"
+message = "\"To live is the rarest thing in the world.\n Most people exist, that's all.\"\n"
+print(famous_person + " once said, " + message)
