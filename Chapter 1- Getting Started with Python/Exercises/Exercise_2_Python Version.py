6b6e1e052b094a7f4c20e088dace48a71b842b0b
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-15 15:56:51 +0400

    Create Exercise_2_Python Version.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_2_Python Version.py
@@ -0,0 +1,9 @@
+## Solutions for Chapter 1: Getting Started with Python
+
+# Exercise 2: Print the Version of Python
+# Write a Python program to get the Python version you are using.
+
+import sys #system-specific function
+print("\nYour current version of Python is: ", sys.version) #prints the version
+print("Information of the version: ", sys.version_info) #prints details of the version
+print()
