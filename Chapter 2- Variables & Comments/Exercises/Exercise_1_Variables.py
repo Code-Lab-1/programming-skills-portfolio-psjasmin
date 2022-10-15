648f26b504c93191cecca5f0fc9e46becb59d368
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-15 15:51:33 +0400

    Create Exercise_1_Variables.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1_Variables.py
@@ -0,0 +1,10 @@
+## Solutions for Chapter 2: Variables & Comments
+
+# Exercise 1: Variables
+# Type out the code below and add the correct data types to complete the variables
+
+message = "\nGod is Good, All the time.\n"
+print(message)
+
+message = "All the time, God is good.\n"
+print(message)
