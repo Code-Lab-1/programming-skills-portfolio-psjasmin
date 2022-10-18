82a970a194377dcbff6f25be2b8244df74f94c0e
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-18 13:15:56 +0400

    Create Exercise_1_Person.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise_1_Person.py
@@ -0,0 +1,16 @@
+## Solutions for Chapter 5: Dictionaries
+
+# Exercise 1: Person
+# Use a dictionary to store information about a person you know.
+
+dictionary = {
+    "first_name": "\nPrince",
+    "last_name": "Panilagao",
+    "age": 13,
+    "city": "Sharjah\n",
+}
+
+print(dictionary["first_name"])
+print(dictionary["last_name"])
+print(dictionary["age"])
+print(dictionary["city"])
