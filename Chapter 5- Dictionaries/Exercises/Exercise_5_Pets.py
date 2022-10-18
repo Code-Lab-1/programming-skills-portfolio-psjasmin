3ad3162b09ccb03df4b1da93b622633762c7129a
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-18 13:16:53 +0400

    Create Exercise_5_Pets.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise_5_Pets.py
@@ -0,0 +1,48 @@
+## Solutions for Chapter 5: Dictionaries
+
+# Exercise 5: Pets
+# Make several dictionaries, where each dictionary represents a different pet.
+
+# Empty list to store the pets in:
+pets = []
+
+# Dictionary including the kind of animal and the owner’s name:
+pet = {
+    "Name":"Cookie",
+    "Owner":"Elizabeth",
+    "Animal Type":"guinea pig",
+    "Weight": 1.2,
+    "Food it eats":"hays",
+}
+pets.append(pet) #to add at the end of the list
+
+pet = {
+    "Name":"Apollo",
+    "Owner":"Henry",
+    "Animal Type":"dog",
+    "Weight": 25,
+    "Food it eats":"peanut butter",
+}
+pets.append(pet)
+
+pet = {
+    "Name":"Donatello",
+    "Owner":"Ryan",
+    "Animal Type":"turtle",
+    "Weight": 1.8,
+    "Food it eats":"vegetables\n",
+}
+pets.append(pet)
+
+# To display information about each pet:
+print("\n\033[1m\033[1;36;40m ______   ______     ______   ______    ")
+print("/\  == \ /\  ___\   /\__  _\ /\  ___\   ")
+print("\ \  _-/ \ \  __\   \/_/\ \/ \ \___  \  ")
+print(" \ \_\    \ \_____\    \ \_\  \/\_____\ ")
+print("  \/_/     \/_____/     \/_/   \/_____/ \033[0m")
+
+for pet in pets:
+    print("\n\033[1;34;40m+----------------------------------+\033[0m")
+    print(f"\033[1m\033[1;36;40m  Information about {pet['Name'].title()}:\033[0m")
+    for key, value in pet.items():
+        print(f"     {key}: {value}")
