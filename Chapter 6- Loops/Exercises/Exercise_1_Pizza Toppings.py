1a4960a883b09257fad6ecc9887cf6450ebe23b5
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-18 16:07:33 +0400

    Create Exercise_1_Pizza Toppings.py

---
+++ Chapter 6- Loops/Exercises/Exercise_1_Pizza Toppings.py
@@ -0,0 +1,23 @@
+## Solutions for Chapter 6: Loops
+
+# Exercise 1: Pizza Toppings
+# Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
+
+print("\n\033[1;31;40m     ,------. ,--.                       ")
+print("     |  .--. '`--',-----.,-----. ,--,--.")
+print("     |  '--' |,--.`-.  / `-.  / ' ,-.  | ")
+print("     |  | --' |  | /  `-. /  `-.\ '-'  | ")
+print("     `--'     `--'`-----'`-----' `--`--' \033[0m")
+
+print("+--------------------------------------------+\n\033[1m| What topping would you like on your pizza? |\033[0m\n+--------------------------------------------+")
+print("\033[1;30;40m     (Enter 'quit' when you are finished)\033[0m\n")
+
+prompt = ("\033[1m\033[1;31;40mEnter topping: \033[0m")
+
+while True:
+    topping = input(prompt)
+    if topping != 'quit':
+        print(f"  I'll add {topping} to your pizza!\n")
+    else:
+        break
+print()
