00a2cdbcec2f609e664361b8a56ae9d0d788f99a
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-26 15:25:15 +0400

    Create Act2_Star.py

---
+++ Chapter 6- Loops/Exercises/Act2_Star.py
@@ -0,0 +1,9 @@
+# CHAPTER 6: BONUS WORKS
+
+# ----- STAR-TURTLE (FOR LOOP) - 20/10/22 -----
+    # Star shape using for loop
+import turtle
+star = turtle.Turtle()
+for x in range(6):
+    star.forward(100)
+    star.right(144)
