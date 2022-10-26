753d8e357aa8f1cb11c8d792310d2427dd246d3f
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-10-26 15:24:41 +0400

    Create Act1_Hexagon.py

---
+++ Chapter 6- Loops/Exercises/Act1_Hexagon.py
@@ -0,0 +1,9 @@
+# CHAPTER 6: BONUS WORKS
+
+# ----- HEXAGON-TURTLE (FOR LOOP) - 20/10/22 -----
+    # Hexagon shape using for loop
+import turtle
+hexagon = turtle.Turtle()
+for x in range(6):
+    hexagon.forward(90)
+    hexagon.left(300)
