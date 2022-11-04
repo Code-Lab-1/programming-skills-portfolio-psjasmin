e132ebc1c25392c58bfca96c256d36276b4d29ec
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 11:22:42 +0400

    Create Bonus Work_5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Bonus Work_5.py
@@ -0,0 +1,7 @@
+# Bonus Work #5: Asking The User for their Favorite Book - 4/11/22
+# Create a variable named 'favorite_book' and prompt the user to add a variable to it.
+
+print("\n+-------------------------------------------+")
+favorite_book = input(" Enter your favorite book: ")
+print("+-------------------------------------------+")
+print(f"\nWOW! You like {favorite_book} too? You have such amazing taste in books :>\n")
