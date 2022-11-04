7539f46ab1b966f9bbab70cf4b51a11e017c812d
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 10:26:37 +0400

    Create Bonus Work_1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Bonus Work_1.py
@@ -0,0 +1,6 @@
+# Bonus Work #1: Printing Strings - 15/9/22
+# Write a program that will inform the user this is your first python program.
+
+print("\n-----Printing Strings-----\n")
+print("Hello World :)")
+print('This is my first Python program!\n')
