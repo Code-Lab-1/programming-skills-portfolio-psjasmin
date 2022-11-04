3e1201cfb08d8829897d4e251a878dc5da0118d1
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 11:20:55 +0400

    Create Bonus Work_4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Bonus Work_4.py
@@ -0,0 +1,14 @@
+# Bonus Work #4: Getting Details From A User - 11/4/22
+# Take a user's name, age, and the country they live in using various variables.
+
+name = input("\n> What is your name? ")
+print(f"Hi {name}! Nice to meet you ^-^")
+
+age = int(input("\n> How old are you? "))
+if age <= 20:
+    print(f"Wow, you're {age}? You're very young!\n")
+else:
+    print(f"Wow, you're {age}? You're very old!\n")
+
+place = input("> Where do you live? ")
+print(f"You live in such a wonderful place <3\n")
