43558611872cf32580d79d656337d9faf391d94c
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 11:48:42 +0400

    Create Bonus Work_1.py

---
+++ Chapter 3- Data Structures/Exercises/Bonus Work_1.py
@@ -0,0 +1,30 @@
+# Bonus Work #1: Functions of List (Part 1) - 29/9/22
+# Part 1 -- How to Format and Print Lists in a Specific Way
+
+# Format of List
+list_of_names = ["Archer","Atlas","Rhysand","Azriel","Cassian","Feyre","Sam"]
+
+# Printing the whole List
+print("\n-----Printing the Whole List-----")
+print(list_of_names) 
+
+# To print a specific value through index
+print("\n-----Printing a Specific Value through Index-----")
+print(list_of_names[4])
+
+# To print the values from 0-5
+print("\n-----Printing the Values from 0-5-----")
+print(list_of_names[0:5])
+
+# To print the value from the end of a list
+print("\n-----Printing the Value from the Back-----")
+print(list_of_names[-1])
+
+# To only print first and last value
+print("\n-----Printing the First and Last Value-----")
+print(list_of_names[0], list_of_names[5])
+
+# To skip the first two and going to the last
+print("\n-----Skipping the First Two Values and Printing the Last-----")
+print(list_of_names[2:])
+print()
