5b393a02f7199e792d9ca0a5d7d9d8c984721f6e
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 14:41:10 +0400

    Create Bonus Work_1.py

---
+++ Chapter 5- Dictionaries/Exercises/Bonus Work_1.py
@@ -0,0 +1,22 @@
+# Bonus Work #1: Dictionaries (Part 1) - 19/10/22
+# Part 1 -- Dictionary Format | Printing keys and values
+
+# DICTIONARY SYNTAX
+print("\n-----Printing Dictionaries-----")
+student = {
+    "Name":"Princess", "Age":16, "Class":"Year 1", "Gender":"Female", "Location":"United Arab Emirates",   
+}
+print(student)
+
+# Printing a value
+print("\n-----Printing A Value-----")
+print(student["Location"])
+
+# Printing only the first value (key)
+print("\n-----Printing Keys-----")
+print(student.keys())
+
+# Printing only the second value (value)
+print("\n-----Printing Values-----")
+print(student.values())
+print()
