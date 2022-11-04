4b7cc19076ecd1552eb5158b29f1fe400016d0c1
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 11:49:01 +0400

    Create Bonus Work_3.py

---
+++ Chapter 3- Data Structures/Exercises/Bonus Work_3.py
@@ -0,0 +1,21 @@
+# Bonus Work #3: Functions of List (Part 3) - 29/9/22
+# Part 3 -- Reverse Function | Concatenating Lists | Sort Function
+
+list_of_names = ["Archer","Atlas","Rhysand","Azriel","Cassian","Feyre","Sam"]
+
+# Reverse Function
+print("\n----Reverse Function-----")
+list_of_names.reverse()
+print(list_of_names)
+
+# Concatenating Lists
+print("\n----Concatening Lists-----")
+age = ["35", "40", "16", "8"]
+print(list_of_names + age)
+
+# Sort Function
+print("\n----Sort Function-----")
+print(age)
+age.sort()
+print(age)
+print()
