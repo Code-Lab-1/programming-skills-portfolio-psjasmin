e1f43e180a0dee4904a664ce0c74c8e33768c29a
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-04 10:27:59 +0400

    Create Bonus Work_3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Bonus Work_3.py
@@ -0,0 +1,12 @@
+# Bonus Work #3: Adding Colors to Strings - 12/10/22
+
+# Codes for Adding Colors to Python Texts
+print("\n-----Colored Texts-----\n")
+print("\033[1;30;40m --Dark Gray--      \033[0m")            
+print("\033[1;31;40m --Bright Red--     \033[0m")
+print("\033[1;32;40m --Bright Green--   \033[0m")
+print("\033[1;33;40m --Yellow--         \033[0m")
+print("\033[1;34;40m --Bright Blue--    \033[0m")
+print("\033[1;35;40m --Bright Magenta-- \033[0m")
+print("\033[1;36;40m --Bright Cyan--    \033[0m")
+print("\033[1;37;40m --White--          \033[0m\n")
