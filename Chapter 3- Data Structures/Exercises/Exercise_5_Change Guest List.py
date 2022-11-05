8ec0c1c1e9334ca420b7bd1f6ca59062ceb224c8
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-05 11:12:33 +0400

    Update Exercise_5_Change Guest List.py

--- Chapter 3- Data Structures/Exercises/Exercise_5_Change Guest List.py
+++ Chapter 3- Data Structures/Exercises/Exercise_5_Change Guest List.py
@@ -5,7 +5,7 @@
 
 guests = ["Jane Austen", "Suzanne Collins", "Stephen King"]
 
-print("\n\033[1;35;40m.-=-=-=-=-=-=-=-=-=-=-=-.")
+print("\n\033[1;35m.-=-=-=-=-=-=-=-=-=-=-=-.")
 print("|                       |")
 print("!   ⋆｡˚  WE ARE         !")
 print(":       INVITING        :")
@@ -16,20 +16,20 @@ print(".-=-=-=-=-=-=-=-=-=-=-=-. \033[0m")
 
 # \033[1m - makes text bold
 name = guests[0].title()
-print(f"\n\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\n\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
 
 name = guests[1].title()
-print(f"\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
 
 name = guests[2].title()
-print(f"\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
 
-print("\033[1;31;40m" + (guests[1]) + " can't make it to the dinner.\033[0m")
+print("\033[1;31m" + (guests[1]) + " can't make it to the dinner.\033[0m")
 
 guests[1] = "William Shakespeare" # Updating the list
 
 # Updated Guest Invitations
-print("\n\033[1;35;40m.-=-=-=-=-=-=-=-=-=-=-=-.")
+print("\n\033[1;35m.-=-=-=-=-=-=-=-=-=-=-=-.")
 print("|                       |")
 print("!   ⋆｡˚  WE ARE         !")
 print(":       INVITING        :")
@@ -39,10 +39,10 @@ print("|                       |")
 print(".-=-=-=-=-=-=-=-=-=-=-=-. \033[0m")
 
 name = guests[0].title()
-print(f"\n\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\n\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
 
 name = guests[1].title()
-print(f"\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
 
 name = guests[2].title()
-print(f"\033[1m\033[1;36;40mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
+print(f"\033[1m\033[1;36mDear {name},\033[0m\n""Gourmet delights and an evening of culinary adventure await you at the home of Mr. Moriarty at 33 Petunia Way, Birmingham.\n˗ˏˋ Please join us for drinks, followed by dinner on the 20th of November at 7:00 pm.´ˎ˗\n")
