084fe53ba654d74f7ff4260f260da976b2a0d0c1
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-05 11:05:35 +0400

    Update Bonus Work_3.py

--- Chapter 1- Getting Started with Python/Exercises/Bonus Work_3.py
+++ Chapter 1- Getting Started with Python/Exercises/Bonus Work_3.py
@@ -1,12 +1,12 @@
 # Bonus Work #3: Adding Colors to Strings - 12/10/22
 
 # Codes for Adding Colors to Python Texts
-print("\n-----Colored Texts-----\n")
-print("\033[1;30;40m --Dark Gray--      \033[0m")            
-print("\033[1;31;40m --Bright Red--     \033[0m")
-print("\033[1;32;40m --Bright Green--   \033[0m")
-print("\033[1;33;40m --Yellow--         \033[0m")
-print("\033[1;34;40m --Bright Blue--    \033[0m")
-print("\033[1;35;40m --Bright Magenta-- \033[0m")
-print("\033[1;36;40m --Bright Cyan--    \033[0m")
-print("\033[1;37;40m --White--          \033[0m\n")
+print("\n\n\033[1m-----Colored Texts-----\033[0m\n")
+print("\033[1;30m --Dark Gray--      \033[0m")            
+print("\033[1;31m --Bright Red--     \033[0m")
+print("\033[1;32m --Bright Green--   \033[0m")
+print("\033[1;33m --Yellow--         \033[0m")
+print("\033[1;34m --Bright Blue--    \033[0m")
+print("\033[1;35m --Bright Magenta-- \033[0m")
+print("\033[1;36m --Bright Cyan--    \033[0m")
+print("\033[1;37m --White--          \033[0m\n")
