7756a542a044a5a3a9c1b3d931d83b239e06ed53
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-06 18:21:17 +0400

    Update Exercise_4_Rivers.py

--- Chapter 5- Dictionaries/Exercises/Exercise_4_Rivers.py
+++ Chapter 5- Dictionaries/Exercises/Exercise_4_Rivers.py
@@ -10,19 +10,19 @@ rivers = {
 }
 
 # Loop to print a sentence about each river:
-print("\n\033[1;32;40m+---------------------------------------------------+\033[0m")
+print("\n\033[1;32m+---------------------------------------------------+\033[0m")
 for word, definition in rivers.items():
-    print(f"The \033[1m\033[1;32;40m{word.title()} river \033[0mruns through {definition}.")
-print("\033[1;32;40m+---------------------------------------------------+\033[0m\n")
+    print(f"The \033[1m\033[1;32m{word.title()} river \033[0mruns through {definition}.")
+print("\033[1;32m+---------------------------------------------------+\033[0m\n")
 
 # Loop to print the name of each river included in the dictionary:
-print("\033[1;33;40m+---------------------------+\033[0m\n  \033[1mRivers in the dictionary: \033[0m")
+print("\033[1;33m+---------------------------+\033[0m\n  \033[1mRivers in the dictionary: \033[0m")
 for word in rivers.keys():
     print(f" > {word.title()}")
-print("\033[1;33;40m+---------------------------+\n\033[0m")
+print("\033[1;33m+---------------------------+\n\033[0m")
 
 # Loop to print the name of each country included in the dictionary:
-print("\033[1;36;40m+------------------------------+\033[0m\n  \033[1mCountries in the dictionary: \033[0m")
+print("\033[1;36m+------------------------------+\033[0m\n  \033[1mCountries in the dictionary: \033[0m")
 for definition in rivers.values():
     print(f" > {definition.title()}")
-print("\033[1;36;40m+------------------------------+\033[0m\n")
+print("\033[1;36m+------------------------------+\033[0m\n")
