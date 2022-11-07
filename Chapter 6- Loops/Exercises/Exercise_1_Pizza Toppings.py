68f4ca6f8ac757fe8b90f4bbae11300445d61161
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-07 17:05:31 +0400

    Update Exercise_1_Pizza Toppings.py

--- Chapter 6- Loops/Exercises/Exercise_1_Pizza Toppings.py
+++ Chapter 6- Loops/Exercises/Exercise_1_Pizza Toppings.py
@@ -3,16 +3,16 @@
 # Exercise 1: Pizza Toppings
 # Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
 
-print("\n\033[1;31;40m     ,------. ,--.                       ")
+print("\n\033[1;31m     ,------. ,--.                       ")
 print("     |  .--. '`--',-----.,-----. ,--,--.")
 print("     |  '--' |,--.`-.  / `-.  / ' ,-.  | ")
 print("     |  | --' |  | /  `-. /  `-.\ '-'  | ")
 print("     `--'     `--'`-----'`-----' `--`--' \033[0m")
 
 print("+--------------------------------------------+\n\033[1m| What topping would you like on your pizza? |\033[0m\n+--------------------------------------------+")
-print("\033[1;30;40m     (Enter 'quit' when you are finished)\033[0m\n")
+print("\033[1;30m     (Enter 'quit' when you are finished)\033[0m\n")
 
-prompt = ("\033[1m\033[1;31;40mEnter topping: \033[0m")
+prompt = ("\033[1m\033[1;31mEnter topping: \033[0m")
 
 while True:
     topping = input(prompt)
