9e68472c84989a1e3f60ada6597c76f1dff906e8
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-07 17:33:53 +0400

    Update Exercise_5_Cities.py

--- Chapter 7- Functions/Exercises/Exercise_5_Cities.py
+++ Chapter 7- Functions/Exercises/Exercise_5_Cities.py
@@ -7,8 +7,8 @@ def describe_city(city_name, country="Philippines"):
     """Printing Cities"""
     print(f"""- {city_name} is in the {country}.""")
 
-print(f"\n\033[1;32;40m╔══════════════════════════════════════════════════╗\033[0m")
+print(f"\n\033[1;32m╔══════════════════════════════════════════════════╗\033[0m")
 describe_city("Makati")
 describe_city("Ras Al Khaimah", "United Arab Emirates")
 describe_city("Cebu City")
-print("\033[1;32;40m╚══════════════════════════════════════════════════╝\n\033[0m")
+print("\033[1;32m╚══════════════════════════════════════════════════╝\n\033[0m")
