fd35ec6ca2d3557af52ead296834029e9f55dadb
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2022-11-07 17:06:09 +0400

    Update Exercise_5_No Pastrami.py

--- Chapter 6- Loops/Exercises/Exercise_5_No Pastrami.py
+++ Chapter 6- Loops/Exercises/Exercise_5_No Pastrami.py
@@ -9,19 +9,19 @@ sandwich_orders = [
 finished_sandwiches = []
 
 # Removing Pastrami:
-print("\n\033[1;31;40m I'm sorry, the deli ran out of pastrami :(\033[0m")
+print("\n\033[1;31m I'm sorry, the deli ran out of pastrami :(\033[0m")
 while 'Pastrami' in sandwich_orders:
     sandwich_orders.remove('Pastrami')
 
 # Sandwich Orders:
-print("\n\033[1;33;40m ===================== \n  Sandwich Orders: 🥪  \n\033[1;33;40m ===================== \033[0m\n")
+print("\n\033[1;33m ===================== \n  Sandwich Orders: 🥪  \n\033[1;33m ===================== \033[0m\n")
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
     print(f"> I made your {current_sandwich} sandwich")
     finished_sandwiches.append(current_sandwich)
 
 # Finished Sandwiches:
-print("\n\033[1;32;40m ========================= \n  \033[1mFinished Sandwiches: 🥪 \033[0m\n\033[1;32;40m ========================= \033[0m\n")
+print("\n\033[1;32m ========================= \n  \033[1mFinished Sandwiches: 🥪 \033[0m\n\033[1;32m ========================= \033[0m\n")
 for sandwich in finished_sandwiches:
     print(f"> {sandwich} sandwich is done!")
 print()
